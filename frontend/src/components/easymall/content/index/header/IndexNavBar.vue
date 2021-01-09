c1533c9595f07b44106c09e70b6cd83b0fa2a51d
2293736867 <2293736867@qq.com>
2021-01-10 01:42:01 +0800

    4.0

--- frontend/src/components/easymall/content/index/header/IndexNavBar.vue
+++ frontend/src/components/easymall/content/index/header/IndexNavBar.vue
@@ -1,43 +1,46 @@
 <template>
     <el-menu ref="menu" :default-active="activeIndex" mode="horizontal" @select="handleSelect">
         <el-menu-item index="0">首页</el-menu-item>
-        <el-menu-item v-for="i in categories" :index="i.index">{{i.value}}</el-menu-item>
+        <el-menu-item v-for="i in categories" :index="i.index">{{ i.value }}</el-menu-item>
     </el-menu>
 </template>
 
 <script>
+import {onMounted,defineComponent} from 'vue'
 import router from "../../../../../js/router/router";
 import Categories from "../../../../../js/constant/Categories";
 
-export default {
+export default defineComponent({
     name: "IndexNavBar",
-    data(){
-        return {
-            activeIndex:"0",
-            links:[],
-            categories:[],
+    setup() {
+        const activeIndex = "0"
+        const categories = Categories
+        let links = []
+
+        onMounted(_ => {
+            links.push('/')
+            for (let i = 0; i < Categories.length; ++i) {
+                links.push('/products/' + Categories[i].index)
+            }
+        })
+
+        return{
+            activeIndex,
+            links,
+            categories
         }
     },
-    mounted(){
-        this.links.push('/')
-        this.categories = Categories
-        for(let i=0;i<Categories.length;++i)
-        {
-            this.links.push('/products/'+Categories[i].index)
-        }
-    },
-    methods:{
-        handleSelect(key){
-            if(key !== 0)
-            {
-                this.$store.commit('categoryId',key)
+    methods: {
+        handleSelect(key) {
+            if (key !== 0) {
+                this.$store.commit('categoryId', key)
             }
             router.push({
-                path:this.links[key]
+                path: this.links[key]
             })
         },
     }
-}
+})
 </script>
 
 <style scoped>
